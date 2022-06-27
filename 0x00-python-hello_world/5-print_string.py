6505fa44cc152224dea5174c5f9c7c06839955c9
Workukalkidan <workukalkidanayele@gmail.com>
2022-06-27 23:23:19 +0300

    check cycle

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton School"
-value_3_times= word*3
-word_9 = word[0:9]
-print(format(value_3_times))
-print('\n')
-print(format(word_9))
-print('\n')
-
+str = "Holberton School"
+print(str*3)
+print(str[:9])
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,4 +1,4 @@
-cat#ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
@@ -13,8 +13,8 @@ cat#ifndef LISTS_H
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -22,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif 
+#endif /* LISTS_H */

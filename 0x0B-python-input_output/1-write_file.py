afbead3eef89539881e012a05acf14277214ce27
Workukalkidan <workukalkidanayele@gmail.com>
2022-08-02 14:24:32 +0300

    tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-function that writes a string to a text file (UTF8) and returns the number of characters written
-"""
+"""write file"""
 
 
 def write_file(filename="", text=""):
-    """ module write_file
-    """
-    with open(filename, 'w') as f:
-        return f.write(text)
+    """"write file"""
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-''' function that returns an object (Python data structure) represented by a JSON string
-'''
-
+"""from json"""
 import json
 
 
 def from_json_string(my_str):
-    ''' module from_json_string
-     returns Python objects
-    '''
+    """from json string"""
     return json.loads(my_str)
